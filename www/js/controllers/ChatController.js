b723a47f3559521a14dac6b75381b63aa16b6f50
Michal <taramichal@gmail.com>
2016-04-19 21:39:50 +0100

    w trakcie chat, socket.io, template chat

--- www/js/controllers/ChatController.js
+++ www/js/controllers/ChatController.js
@@ -1,63 +1,45 @@
 /**
  * Created by pc on 2016-04-17.
  */
-angular.module('pmApp.ChatCtrl', ['btford.socket-io'])
+angular.module('pmApp.ChatCtrl', ['monospaced.elastic'])
 
-.value('nickName', 'anonymous')
+.controller('ChatController', function($scope, $log, SERVER) {
 
-.controller('ChatController', function($scope, $log, chatSocket, messageFormatter, nickName) {
-
-  this.nickName = nickName;
-  this.messageLog = 'Ready to chat!';
-
-    this.sendMessage = function() {
-
-      console.log($scope);
-
-      var match = this.message.match('^\/nick (.*)');
+  var socket = io.connect(SERVER.url); // uzycie server.url jako constant zamiast IP/localhost pomoglo usunac problem z access-control origin !!!!!!!!!!!!!!!
 
 
-      if(angular.isDefined(match) && angular.isArray(match) && match.length === 2) {
 
-      var oldNick = nickName;
-      nickName = match[1];
-        this.message = '';
-        this.messageLog = messageFormatter(new Date(), nickName, 'nickname changed - from ' + oldNick + ' to ' + nickName + '!') + this.messageLog;
-        this.nickName = nickName;
+  socket.on('connect_error', function(data) {
+    console.log('connect error');
+    console.log(data);
+  });
 
-    }
+  socket.on('connect', function() {
+    console.log('connected');
+  });
 
-    $log.debug('sending message', this.message);
-
-    chatSocket.emit('message', nickName, this.message);
-    $log.debug('message sent', this.message);
-    this.message = '';
+  socket.on('chat message', function(message) {
+    console.log('message z serwera: %s', message);
+  });
 
 
-    };
+  $scope.$watch('chatInput', function(newValue, oldValue) {
+    console.log('chatInput $watch, newValue ' + newValue);
+    if(!newValue) newValue = '';
+  });
+
+  this.submitChatMsg = function(chatForm) {
+
+    console.log($scope);
+
+    console.log(chatForm);
+
+    $scope.chatLog.value = "test";
 
 
-    $scope.$on('socket:broadcast', function(event, data) {
+    socket.emit('chat message', this.chatInput);
 
-      console.log('dostalem broadcast');
-
-        $log.debug('got a message', event.name);
-
-        if(!data.payload) {
-          $log.error('Invalid message', 'event', event,
-                        'data', JSON.stringify(data));
-          return;
-        }
-
-        $scope.$apply(function() {
-
-          this.messageLog = messageFormatter(
-              new Date(), data.source,
-              data.payload) + this.messageLog;
-
-        });
-
-    });
+  }
 
 
 });
--- www/js/services/socketFactory.js
+++ www/js/services/socketFactory.js
@@ -1,14 +1,44 @@
 /**
- * Created by pc on 2016-04-18.
+ * Created by pc on 2016-04-19.
  */
 
 angular.module('pmApp.ChatCtrl')
 
-.factory('chatSocket', function(socketFactory) {
+.factory('socket', function($rootScope, SERVER) {
 
-  var socket = socketFactory();
-  socket.forward('broadcast');
-  return socket
+  var socket = io.connect(SERVER.url); // uzycie server.url jako constant zamiast IP/localhost pomoglo usunac problem z access-control origin !!!!!!!!!!!!!!!
 
+  socket.on('connect', function() {
+    console.log('socket connected');
+  });
+
+  socket.on('connect_error', function(data) {
+    console.log('socket connect error');
+    console.log(data);
+  });
+
+  return {
+
+    on: function (eventName, callback) {
+      socket.on(eventName, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          callback.apply(socket, args);
+        });
+      });
+    },
+    
+    emit: function (eventName, data, callback) {
+      socket.emit(eventName, data, function () {
+        var args = arguments;
+        $rootScope.$apply(function () {
+          if (callback) {
+            callback.apply(socket, args);
+          }
+        });
+      })
+    }
+
+  }
 
 });
